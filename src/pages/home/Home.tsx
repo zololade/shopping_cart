9c1a0a08554c35c1f90555b566f1e3b6497af556
Ololade <73138104+zololade@users.noreply.github.com>
2026-09-01 21:59:10 +0100

    Add Home.css for styling the home page layout and images

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,14 @@
+import shopper from "../../assets/shopper.jpeg";
+
+import "./Home.css";
+
 function Home() {
-  return <div>home</div>;
+  return (
+    <section className="home">
+      <h1>Home</h1>
+
+      <img src={shopper} alt="Man pushing a cart" width={1091} height={976} />
+    </section>
+  );
 }
 export { Home };
