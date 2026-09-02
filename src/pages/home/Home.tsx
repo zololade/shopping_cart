ed6af249c87c18f1c0bbe016b464b703a20daf3d
Ololade <73138104+zololade@users.noreply.github.com>
2026-09-02 17:30:33 +0100

    feat: Revamp Home Page and Update Global Styles
    - Updated Home component to include a welcoming message and a "Shop Now" link.
    - Changed the main image to a new shopping SVG asset.
    - Added a useEffect hook to manage body class for home page styling.
    - Refactored global CSS to improve layout and typography, including a new radial gradient background for the home page.
    - Introduced a new variables.css file for design tokens and color schemes.
    - Added new SVG assets for homeBlob and shopping illustrations.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,29 @@
-import shopper from "../../assets/shopper.jpeg";
+import { useEffect } from "react";
+import { Link } from "react-router";
+
+import shopper from "../../assets/shopping.svg";
 
 import "./Home.css";
 
 function Home() {
+  useEffect(() => {
+    document.body.classList.add("home-active");
+
+    return () => {
+      document.body.classList.remove("home-active");
+    };
+  }, []);
+
   return (
     <section className="home">
-      <h1>Home</h1>
-
-      <img src={shopper} alt="Man pushing a cart" width={1091} height={976} />
+      <div className="intro">
+        <h1>Welcome to our online store</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, ipsam.</p>
+        <Link to="shop">Shop Now</Link>
+      </div>
+      <div className="imgContainer">
+        <img src={shopper} alt="Man pushing a cart" width={1091} height={976} />
+      </div>
     </section>
   );
 }
