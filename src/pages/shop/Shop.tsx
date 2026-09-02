f96dab31920093c1e00a97f25b45708f0dff17ab
Ololade <73138104+zololade@users.noreply.github.com>
2026-09-02 19:35:58 +0100

    feat: implement loading and error handling in Shop component

--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -20,7 +20,23 @@ export interface ProductResponse {
 function Shop() {
   const { data, error, isLoading } = useFetch<ProductResponse>("https://fakestoreapi.com/products");
 
-  console.log(data, error, isLoading);
-  return <section className="shop">shop</section>;
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <section className="shop">
+      <h1>shop</h1>
+      {data?.map((val) => (
+        <article key={val.id}>
+          <img src={val.image} alt={val.title} />
+          <h2>{val.title}</h2>
+          <p>{val.description}</p>
+          <span>{val.category}</span> <span>{val.price}</span> <span>{val.rating.rate}</span>{" "}
+          <span>{val.rating.count}</span>
+        </article>
+      ))}
+    </section>
+  );
 }
 export { Shop };
